4df4292256fa5d26c63322427a16acb8e0749f66
ko-to-mi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-01-28 11:51:30 +0900

    配列操作

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import "./styles.css";
+/** */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = 'varhensu';
+// console.log(val1);
+
+// val1 = 'henkou';
+// console.log(val1);
+
+// var val1 = '再宣言';
+// console.log(val1);
+
+// let val2 = 'lethensu';
+// console.log(val2);
+
+// val2 = '上書き';
+// console.log(val2);
+
+// const val3 = 'constdayo';
+// console.log(val3);
+
+// const val4 = {
+//   name: 'owawa',
+//   age: 30
+// };
+// val4.name = 'bbbb';
+// val4.addres = 'tokyo';
+// console.log(val4)
+
+const val5 = ["dog", "cat"];
+val5[0] = "panda";
+val5.push("bird");
+console.log(val5);
