cc8fc1b4cfaf273781a706e40a864cba20d99ebd
ko-to-mi-san <47980784+ko-to-mi-san@users.noreply.github.com>
2022-01-29 22:57:30 +0900

    スプレッド構文について

--- src/index.js
+++ src/index.js
@@ -72,5 +72,29 @@
 // const message4 = `名前は${name}です。年齢は${age}です。`
 // console.log(message4);
 
-const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
-sayHello();
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
+// sayHello();
+
+//
+
+// const arr2 = [1,2,3,4,5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+
+// arr8[0] = 100;
+// console.log(arr8);
